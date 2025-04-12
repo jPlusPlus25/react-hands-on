09b73fa8ef28617adbd5f19ef780e417e9034c14
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-12 21:48:30 +0530

    udated producttab

--- basic-react-app/src/Producttab.jsx
+++ basic-react-app/src/Producttab.jsx
@@ -1,10 +1,10 @@
 import Product  from "./Product.jsx";
 function Producttab(){
     let options = ["hi-tech","durable","fast"];
-    let option2 = {a:"hi-tech",b:"durable",c:"fast"};
+    
     return(
         <>
-        <Product Title="phone" price="30,000" features={options} features2={option2}/>
+        <Product Title="phone" price="30,000" features={options}/>
         <Product Title="laptop" price="40,000"/>
         <Product Title="pen" price={1}/>
         </>
