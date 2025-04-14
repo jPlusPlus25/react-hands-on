bf740f0f77ee8111a5fac92023c1471fb6684188
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-14 20:16:56 +0530

    new  update

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,15 +1,17 @@
 import Product  from "./Product.jsx";
 import Price from "./Price.jsx";
 import Producttab from "./Producttab.jsx";
+import Button from "./Button.jsx";
 
 
 function App(){
     
     return(
-        <>
-        <center> <h2>Blockbuster Deals | Shop Now</h2></center>
-        <Producttab />
-        </>
+        
+        /*<center> <h2>Blockbuster Deals | Shop Now</h2></center>
+        { <Producttab /> }*/
+        
+        <Button/>
     );
 }
 export default App;
--- basic-react-app/src/Button.jsx
+++ basic-react-app/src/Button.jsx
@@ -1,14 +1,21 @@
-function printHello(){
+function handleClick(){
     console.log("Hello")
 }
-function printBye(){
+function handleMouseOver(){
     console.log("bye")
+}
+function handleDblclick(){
+    console.log("you double click")
 }
 export default function Button(){
     return(
         <div>
-            <button onClick={printHello}>Click me!</button>
-            <p onClick={printBye}>this is parah is for event demo</p>
+            <button onClick={handleClick}>Click me!</button>
+         
+            <p onMouseOver={handleMouseOver}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio fuga veniam nostrum provident 
+                accusamus itaque aperiam cumque suscipit laudantium culpa deleniti nemo, molestiae illo aut modi reiciendis!
+                 Rem, ut. Optio.</p>
+                 <button onDoubleClick={handleDblclick}>double click me!</button>
         </div>
     );
 }
