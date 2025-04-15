0d4e7696cb276f62d7e86816407d5d54e3234136
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-15 11:37:32 +0530

    added LikeButton

--- react-state-class/src/App.jsx
+++ react-state-class/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-
+import Counter from "./Counter";
+import LikeButton from "./LikeButtton";
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-    <h1>States in  React</h1>
+    <h1>States in React</h1>
+    <LikeButton/>
     </>
       
   )
