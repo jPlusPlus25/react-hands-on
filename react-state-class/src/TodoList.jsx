304add55014f1b02daff213ba2084a41ff8a403d
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-04-28 21:31:45 +0530

    some update

--- react-state-class/src/TodoList.jsx
+++ react-state-class/src/TodoList.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 
 export default function TodoList(){
-    let [todos,setTodos]=useState([{task:"sample-task" , id: uuidv4()}]);
+    let [todos,setTodos]=useState([{task:"sample-task" , id: uuidv4(),isDone:false}]);
     let [newTodo,setNewTodo]=useState("");
     
     let addNewTask=()=>{
        setTodos((prevTodos)=>{
-        return [...prevTodos,{task:newTodo,id:uuidv4()}]
+        return [...prevTodos,{task:newTodo,id:uuidv4(),isDone:false}]
        });
        setNewTodo("");
     }; 
@@ -26,13 +26,13 @@ export default function TodoList(){
         }))
       );
     };
-    let UpperCaseone=(id)=>{
+    let markAsDone=(id)=>{
         setTodos( (prevTodos)=>
             prevTodos.map((todo)=>{
              if(todo.id==id){
                 return {
                  ...todo,
-                 task:todo.task.toUpperCase(),
+                 isDone:true,
                  };
                 }else{
                     return todo;
@@ -58,9 +58,9 @@ export default function TodoList(){
         <ul>
            { todos.map((todo) => (
                 <li key={todo.id}>
-                   <span> {todo.task}</span>
+                   <span style={todo.isDone ? {textDecorationLine:"line-through"}:{}}> {todo.task}</span>
                    <button onClick={()=>deleteTodo(todo.id)}>delete</button>
-                   <button onClick={()=>UpperCaseone(todo.id)}>UpperCase One</button>
+                   <button onClick={()=>markAsDone(todo.id)}>Mark As Done</button>
                 </li>
             ))}
         </ul>
